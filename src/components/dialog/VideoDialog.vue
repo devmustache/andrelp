9c69fb01653f60af8c8cba3683a05d60b2b5f3fa
Marco Túlio <mt.fonseca23@gmail.com>
2025-11-23 17:27:57 -0300

    tentativa de player funcional no mobile2

--- src/components/dialog/VideoDialog.vue
+++ src/components/dialog/VideoDialog.vue
@@ -3,7 +3,7 @@
     <div
       v-show="$show.show && $show.id === video_id"
       class="bg-dark-blur z-1000 pointer-events-auto fixed inset-0 grid w-full cursor-pointer place-items-center"
-      @click="pauseVideo()"
+      @click="pauseVideo"
     >
       <div @click.stop class="container-md relative">
         <div class="overflow-hidden rounded shadow-xl relative">
@@ -23,7 +23,7 @@
 
         <button
           class="btn btn-icon surface-dark btn-absolute -right-1 -top-1 z-10 grid h-10 w-10 place-items-center"
-          @click="pauseVideo()"
+          @click="pauseVideo"
         >
           <slot />
         </button>
@@ -54,72 +54,69 @@ let Plyr;
 // Detecta mobile
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-// Função para abrir modal e tocar vídeo (chamada pelo clique do usuário)
-export const openVideo = async () => {
+// Função para abrir modal e tocar vídeo
+const openVideo = async () => {
   showVideo.set({ id: props.video_id, show: true });
   disableBodyScroll(document.body);
-  await playVideo();
+
+  if (!videoPlayer.value) {
+    await initPlayer();
+  } else {
+    // Se já existe player, apenas play
+    loading.value = false;
+    videoPlayer.value.play().catch(() => {});
+  }
 };
 
 const pauseVideo = () => {
   showVideo.set({ id: $show.value.id, show: false });
 
-  if (isMobile && videoPlayer.value) {
-    videoPlayer.value.destroy();
-    videoPlayer.value = null;
-    container.value.innerHTML = ""; // limpa iframe
-    loading.value = false;
-  } else if (videoPlayer.value) {
+  if (videoPlayer.value) {
     videoPlayer.value.pause();
   }
 
   enableBodyScroll(document.body);
 };
 
-const playVideo = async () => {
+// Inicializa o player apenas uma vez
+const initPlayer = async () => {
   if (!Plyr) Plyr = (await import("plyr")).default;
 
-  if (!videoPlayer.value) {
-    if (isMobile) loading.value = true;
+  loading.value = isMobile;
 
-    videoPlayer.value = new Plyr(container.value, {
-      playsinline: isMobile ? true : 0,
-      settings: ["loop"],
-      iconUrl: "/icons/plyr.svg",
-      controls: [
-        "play",
-        "progress",
-        "current-time",
-        "mute",
-        "volume",
-        "airplay",
-        "fullscreen",
-      ],
-      youtube: {
-        origin: window.location.host,
-        iv_load_policy: 3,
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        enablejsapi: 1,
-        noCookie: true,
-      },
+  videoPlayer.value = new Plyr(container.value, {
+    playsinline: isMobile ? true : 0,
+    settings: ["loop"],
+    iconUrl: "/icons/plyr.svg",
+    controls: [
+      "play",
+      "progress",
+      "current-time",
+      "mute",
+      "volume",
+      "airplay",
+      "fullscreen",
+    ],
+    youtube: {
+      origin: window.location.host,
+      iv_load_policy: 3,
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      enablejsapi: 1,
+      noCookie: true,
+    },
+  });
+
+  if (isMobile) {
+    videoPlayer.value.on("canplay", () => {
+      videoPlayer.value.play().catch(() => {});
+      loading.value = false;
     });
-
-    if (isMobile) {
-      // Garante autoplay no mobile dentro da interação do usuário
-      videoPlayer.value.on("canplay", () => {
-        videoPlayer.value.play().catch(() => {});
-        loading.value = false;
-      });
-    } else {
-      // Desktop continua usando ready
-      videoPlayer.value.on("ready", () => {
-        videoPlayer.value.play();
-      });
-    }
   } else {
-    videoPlayer.value.play();
+    videoPlayer.value.on("ready", () => {
+      videoPlayer.value.play();
+    });
   }
 };
 
