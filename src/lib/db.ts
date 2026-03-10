2db762e43ead2d5de3edc7f664aa8c4375446c97
Marco Túlio <mt.fonseca23@gmail.com>
2026-03-10 15:10:58 -0300

    fix: make db connection safe by setting dummy defaults if envs are missing

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: import.meta.env.UPSTASH_REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_URL,
-  token: import.meta.env.UPSTASH_REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: import.meta.env.UPSTASH_REDIS_REST_URL || process.env.UPSTASH_REDIS_REST_URL || 'https://localdummy.upstash.io',
+  token: import.meta.env.UPSTASH_REDIS_REST_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN || 'dummy_token',
 })
